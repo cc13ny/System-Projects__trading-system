c4fb8f26b67365a98a401b4b95c8cd1247a67ac6
Chi Chen <chenchiapply@gmail.com>
2017-03-29 01:20:16 -0400

    Initialize project with examples using MySQL and Concurrency

---
+++ cmake-build-debug/CMakeFiles/trading_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trading_system.dir/main.cpp.o"
+  "trading_system.pdb"
+  "trading_system"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trading_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
